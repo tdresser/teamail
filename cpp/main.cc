2de8440ba192bb8ad5651ff57226e3dd9806c746
Tim Dresser <tdresser@gmail.com>
2021-04-25 08:39:51 -0400

    Clang format

--- cpp/main.cc
+++ cpp/main.cc
@@ -1,5 +1,5 @@
-#include <emscripten/val.h>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
 #include <stdio.h>
 #include <string>
 
@@ -7,46 +7,37 @@ using namespace emscripten;
 
 using string = std::string;
 
-string getStringCC()
-{
+string getStringCC() {
   val window = val::global("window");
   val str = window.call<val>("getString");
 
   return str.as<string>();
 }
 
-struct PointS
-{
+struct PointS {
   float x;
   float y;
 };
 
-class Point : public PointS
-{
-public:
-  Point(float x, float y)
-  {
+class Point : public PointS {
+ public:
+  Point(float x, float y) {
     PointS::x = x;
     PointS::y = y;
   };
 };
 
-extern "C" PointS getCardOffset()
-{
+extern "C" PointS getCardOffset() {
   return Point(100, 120);
 }
 
-extern "C" int testGetString()
-{
+extern "C" int testGetString() {
   printf("%s\n", getStringCC().c_str());
   return 7;
 }
 
-EMSCRIPTEN_BINDINGS(geometry)
-{
-  value_object<PointS>("Point")
-      .field("x", &PointS::x)
-      .field("y", &PointS::y);
+EMSCRIPTEN_BINDINGS(geometry) {
+  value_object<PointS>("Point").field("x", &PointS::x).field("y", &PointS::y);
 
   function<PointS>("getCardOffset", &getCardOffset);
   function<int>("testGetString", &testGetString);
