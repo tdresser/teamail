ff96b4e49b4c95907af7a84d4297f5f58ef2c9c6
Timothy Dresser <tdresser@gmail.com>
2021-04-26 07:28:45 -0400

    eslint progress

--- web/build.ts
+++ web/build.ts
@@ -1,15 +1,17 @@
 const serverConfig = {
   servedir: __dirname,
-  port: 3000
-}
+  port: 3000,
+};
 
-require('esbuild').serve(serverConfig, {
-  entryPoints: ['src/index.tsx', 'src/Bindings.ts'],
-  bundle: true,
-  outdir: 'build',
-  target: 'esnext',
-  sourcemap: 'external',
-}).catch((e) => {
-  console.log(e);
-  process.exit(1)
-});
+require('esbuild')
+  .serve(serverConfig, {
+    entryPoints: ['src/index.tsx', 'src/Bindings.ts'],
+    bundle: true,
+    outdir: 'build',
+    target: 'esnext',
+    sourcemap: 'external',
+  })
+  .catch((e: Error) => {
+    console.log(e);
+    process.exit(1);
+  });
